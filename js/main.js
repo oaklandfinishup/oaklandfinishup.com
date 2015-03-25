67c7ae8c488d1dbd462b0ee1c51da178976fc60b
Seemant Kulleen <seemantk@gmail.com>
2015-03-25 01:19:28 -0700

    Modularized.  Also, changed the theme to one inspired by: http://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/ Also, added a twitter feed.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function (){
+
+  // create a LatLng object containing the coordinate for the center of the map
+  var latlng = new google.maps.LatLng(37.795665,-122.276998);
+
+  // prepare the map properties
+  var options = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  // initialize the map object
+  var map = new google.maps.Map(document.getElementById('google_map'), options);
+
+  // add Marker
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  // add listener for a click on the pin
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  // add information window
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong>The Port Workspaces</strong><br><br>101 Broadway<br> Jack London Square, Oakland, CA</div>'
+  });  
+});
+
