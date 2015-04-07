e2795d27db7a4b289b7b2bc908f4cecec7ebea87
Seemant Kulleen <seemantk@gmail.com>
2015-04-06 18:13:56 -0700

    Get rid of the google map code and replace with a gallery shuffler as found here: http://stackoverflow.com/a/9641564/1968883

--- js/main.js
+++ js/main.js
@@ -17,37 +17,13 @@ $(document).ready(function (){
 		);
   });
 
-
-  // create a LatLng object containing the coordinate for the center of the map
-  var latlng = new google.maps.LatLng(37.795665,-122.276998);
-
-  // prepare the map properties
-  var options = {
-    zoom: 15,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    navigationControl: true,
-    mapTypeControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true
-  };
-
-  // initialize the map object
-  var map = new google.maps.Map(document.getElementById('google_map'), options);
-
-  // add Marker
-  var marker1 = new google.maps.Marker({
-    position: latlng, map: map
-  });
-
-  // add listener for a click on the pin
-  google.maps.event.addListener(marker1, 'click', function() {
-    infowindow.open(map, marker1);
-  });
-
-  // add information window
-  var infowindow = new google.maps.InfoWindow({
-    content:  '<div class="info"><h3>F\'Up Here</h3><strong>The Port Workspaces</strong><br><br>101 Broadway<br> Jack London Square, Oakland, CA</div>'
-  });  
+  // Shuffle the mentor gallery
+  var parent = document.getElementById("mentor-gallery");
+  var divs = parent.children;
+  var frag = document.createDocumentFragment();
+  while (divs.length) {
+  	      frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
+  }
+  parent.appendChild(frag);
 });
 
