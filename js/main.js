e0855b82400dd4e7204f2a888773646cbebb88c1
Seemant Kulleen <seemantk@gmail.com>
2015-04-06 18:14:53 -0700

    Add attribution in code.

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ $(document).ready(function (){
   });
 
   // Shuffle the mentor gallery
+  // see: http://stackoverflow.com/a/9641564/1968883
   var parent = document.getElementById("mentor-gallery");
   var divs = parent.children;
   var frag = document.createDocumentFragment();
