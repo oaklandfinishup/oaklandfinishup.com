6e4566879523c2491b58ecb477142e793b115b3c
Seemant Kulleen <seemantk@gmail.com>
2015-09-01 22:47:30 -0700

    Add carousel initialization for the fullscreen background image slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 $(document).ready(function(){$(".smoothie").on('click',function(event){var target=$(this.hash),navOffset=$('#navbar').height();event.preventDefault();return $('html, body').animate({scrollTop:target.offset().top-navOffset},600,function(){return window.history.pushState(null,null,target.selector);});});});
+$('.carousel').carousel({ interval: 5000 });
+
