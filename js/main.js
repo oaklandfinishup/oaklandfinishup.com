6a49732fed83521c899c51249188dd008664b209
seemant <seemant@funtoo.org>
2023-01-24 05:06:00 -0800

    carousel will css only

--- js/main.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){$(".smoothie").on('click',function(event){var target=$(this.hash),navOffset=$('#navbar').height();event.preventDefault();return $('html, body').animate({scrollTop:target.offset().top-navOffset},600,function(){return window.history.pushState(null,null,target.selector);});});});
-$('.carousel').carousel({ interval: 5000 });
-
